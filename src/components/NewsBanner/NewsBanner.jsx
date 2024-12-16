9fe66d767a28cf1d1d70eaaacd1d9cfef44a90ec
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-16 13:24:01 +0400

    Added project structory and (main, header, banner, news list)

---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {formatTimeAgo} from "../../helpers/formatTimeAgo";
+import styles from "./styles.module.css";
+import Image from "../Image/Image";
+
+const NewsBanner = ({item}) => {
+    return (
+        <div className={styles?.banner}>
+            <Image image={item?.image} />
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author}</p>
+        </div>
+    )
+};
+
+export default NewsBanner;
