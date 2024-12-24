a920193f8579a2abd12fb1dbf050c5aaa13e02b5
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-24 15:43:20 +0400

    Refactoring

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import {formatTimeAgo} from "../../helpers/formatTimeAgo";
+import { formatTimeAgo } from "../../helpers/formatTimeAgo";
 import styles from "./styles.module.css";
 import Image from "../Image/Image";
+import withSkeleton from "../../helpers/hocs/withSkeleton";
 
-const NewsBanner = ({item}) => {
-    return (
-        <div className={styles?.banner}>
-            <Image image={item?.image} />
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author}</p>
-        </div>
-    )
+const NewsBanner = ({ item }) => {
+  return (
+    <div className={styles?.banner}>
+      <Image image={item?.image} />
+      <h1 className={styles.title}>{item.title}</h1>
+      <p className={styles.extra}>
+        {formatTimeAgo(item.published)} by {item.author}
+      </p>
+    </div>
+  );
 };
 
-export default NewsBanner;
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
