c34d3021474093bee672397a98efb6eda9c72af5
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-24 17:42:30 +0400

    Add design for web

---
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./styles.module.css";
+import { getCategories } from "../../api/apiNews";
+import Categories from "../../components/Categories/Categories";
+import Search from "../../components/Search/Search";
+import { useFetch } from "../../helpers/hooks/useFetch";
+
+const NewsFilters = ({ filters, changeFilter }) => {
+  const { data: dataCategories } = useFetch(getCategories);
+  return (
+    <div className={styles.filters}>
+      {dataCategories ? (
+        <Categories
+          categories={dataCategories.categories}
+          selectedCategory={filters.category}
+          setSelectedCategory={(category) => changeFilter("category", category)}
+        />
+      ) : null}
+
+      <Search
+        keywords={filters.keywords}
+        setKeywords={(keywords) => changeFilter("keywords", keywords)}
+      />
+    </div>
+  );
+};
+
+export default NewsFilters;
