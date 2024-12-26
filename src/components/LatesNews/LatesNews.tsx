08bab22c1c556138c6eb78c329a8b12ee3bd9e8f
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-12-26 17:46:05 +0400

    Fixed bugs

--- src/components/LatesNews/LatesNews.tsx
+++ src/components/LatesNews/LatesNews.tsx
@@ -1,11 +1,11 @@
 import styles from "./styles.module.css";
 import BannersList from "../BannersList/BannersList";
-import { getLatestNewa } from "../../api/apiNews";
+import { getLatestNews } from "../../api/apiNews";
 import { NewsApiResponse } from "../../interfaces";
 import { useFetch } from "../../helpers/hooks/useFetch";
 
 const LatesNews = () => {
-  const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNewa);
+  const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
   return (
     <section className={styles.section}>
       <BannersList banners={data && data.news} isLoading={isLoading} />
