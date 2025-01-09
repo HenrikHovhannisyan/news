3b7d95d92dc940611e7f2f60af3c870ea50971d2
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-01-09 16:51:05 +0400

    Added context and change theme

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -5,6 +5,7 @@ import Search from "../../components/Search/Search";
 import { useFetch } from "../../helpers/hooks/useFetch";
 import Slider from "../Slider/Slider";
 import { CategoriesApiResponse, IFilters } from "../../interfaces";
+import { useTheme } from "../../context/ThemeContext";
 
 interface Props {
   filters: IFilters;
@@ -12,13 +13,14 @@ interface Props {
 }
 
 const NewsFilters = ({ filters, changeFilter }: Props) => {
+  const { isDark } = useTheme();
   const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(
     getCategories
   );
   return (
     <div className={styles.filters}>
       {dataCategories ? (
-        <Slider>
+        <Slider isDark={isDark}>
           <Categories
             categories={dataCategories.categories}
             selectedCategory={filters.category}
